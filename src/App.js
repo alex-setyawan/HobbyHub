f831a6839780d7e1ac36691b3abc59a084bd2754
Jeremiah <jeremiahau016@gmail.com>
2023-06-05 21:42:33 +0800

    add GenericTopic component

--- src/App.js
+++ src/App.js
@@ -7,21 +7,25 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Home from "./Home";
-import Archery from "./Archery";
-import Basketball from "./Basketball";
-import Culinary from "./Culinary";
 import NewPost from "./NewPost";
 import SignUp from "./SignUp";
 import LogIn from "./LogIn";
+import GenericTopic from "./GenericTopic";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />}>
         <Route index element={<Home />} />
-        <Route path="/archery" element={<Archery />} />
-        <Route path="/basketball" element={<Basketball />} />
-        <Route path="/culinary" element={<Culinary />} />
+        <Route path="/archery" element={<GenericTopic pageType="Archery" />} />
+        <Route
+          path="/basketball"
+          element={<GenericTopic pageType="Basketball" />}
+        />
+        <Route
+          path="/culinary"
+          element={<GenericTopic pageType="Culinary" />}
+        />
         <Route path="/new-post" element={<NewPost />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/log-in" element={<LogIn />} />
