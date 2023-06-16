33ac3a58aae17bd1d02924658a267c13cab4106c
Jeremiah <jeremiahau016@gmail.com>
2023-06-16 20:08:41 +0800

    Update CSS style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
 import ImgRedirect from "@/components/ImgRedirect";
-import Navbar from "@/components/Navbar";
 import Archery from "../assets/images/archery.jpg";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <h1>Topics</h1>
       <p>
         Which one <u>interests</u> you today?
       </p>
-      <div className="flex gap-3 justify-between">
+      <div className="flex gap-3 justify-between text-slate-50">
         <ImgRedirect img={Archery} text={"Archery"} />
         <ImgRedirect img={Archery} text={"Archery"} />
         <ImgRedirect img={Archery} text={"Archery"} />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,29 @@
 import Link from "next/link";
-import Image from "next/image";
-import hobbyhub from "../assets/images/hobbyhub.png";
-import Redirect from "./Redirect";
+import { ICONS } from "./Icons";
+import { buttonVariants } from "./ui/button";
 
 export default function Navbar() {
   return (
     <>
-      <div className="flex justify-between">
-        <Link href="/">
-          <Image src={hobbyhub} width={200} alt={"hobbyhub icon"} />
-        </Link>
-        <div className="flex gap-1">
-          <Redirect page="login" text="Login" />
-          <Redirect page="signup" text="Sign Up" />
+      <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
+        <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
+          {/* logo */}
+          <Link href="/" className="flex gap-2 items-center">
+            <ICONS.logo />
+            <p className="hidden text-zinc-700 text-sm font-medium sm:block">
+              HobbyHub
+            </p>
+          </Link>
+
+          {/* Log In/Sign Up */}
+          <div className="flex gap-4">
+            <Link href="/sign-in" className={buttonVariants()}>
+              Log In
+            </Link>
+            <Link href="/sign-in" className={buttonVariants()}>
+              Sign Up
+            </Link>
+          </div>
         </div>
       </div>
     </>
