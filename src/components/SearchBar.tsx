c07ae52dc38326cc749c6761580e46b27c03c869
Jeremiah <jeremiahau016@gmail.com>
2023-06-24 17:50:31 +0800

    Add search functionality

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { useCallback, useState } from "react";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "./ui/Command";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Prisma, Topic } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { Users } from "lucide-react";
+import debounce from "lodash.debounce";
+
+export default function SearchBar() {
+  const [input, setInput] = useState<string>("");
+  const router = useRouter();
+
+  const request = debounce(() => {
+    refetch();
+  }, 300);
+
+  const debounceRequest = useCallback(() => {
+    request();
+  }, []);
+
+  const {
+    isFetching,
+    data: queryResults,
+    refetch,
+    isFetched,
+  } = useQuery({
+    queryFn: async () => {
+      if (!input) return [];
+      const { data } = await axios.get(`/api/search?q=${input}`);
+      return data as (Topic & {
+        _count: Prisma.TopicCountOutputType;
+      })[];
+    },
+    queryKey: ["search-query"],
+    enabled: false,
+  });
+
+  return (
+    <Command className="relative rounded-lg border max-w-lg z-50 overflow-visible">
+      <CommandInput
+        value={input}
+        onValueChange={(text) => {
+          setInput(text);
+          debounceRequest();
+        }}
+        className="outline-none border-none focus:border-none focus:outline-none ring-0"
+        placeholder="Search communities..."
+        isLoading={false}
+      />
+
+      {input.length > 0 && (
+        <CommandList className="absolute bg-white top-full inset-x-0 shadow rounded-b-md">
+          {isFetched && <CommandEmpty>No results found.</CommandEmpty>}
+          {(queryResults?.length ?? 0) > 0 ? (
+            <CommandGroup heading="Communities">
+              {queryResults?.map((topic) => (
+                <CommandItem
+                  onSelect={(e) => {
+                    router.push(`/topic/${e}`);
+                    router.refresh();
+                  }}
+                  key={topic.id}
+                  value={topic.name}
+                >
+                  <Users className="mr-2 h-4 w-4" />
+                  <a href={`/topic/${topic.name}`}>{topic.name}</a>
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          ) : null}
+        </CommandList>
+      )}
+    </Command>
+  );
+}
