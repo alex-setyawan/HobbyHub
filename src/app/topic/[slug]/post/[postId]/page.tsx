570984e6377b77006bd5cfc3030c65b370c60de7
Jeremiah <jeremiahau016@gmail.com>
2023-06-24 13:10:02 +0800

    Add comment functionality

--- src/app/topic/[slug]/post/[postId]/page.tsx
+++ src/app/topic/[slug]/post/[postId]/page.tsx
@@ -8,6 +8,8 @@ import { Post, User, Vote } from "@prisma/client";
 import { ArrowBigDown, ArrowBigUp, Loader2 } from "lucide-react";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
+import PostVoteServer from "@/components/post-vote/PostVoteServer";
+import CommentsSection from "@/components/CommentsSection";
 
 type PageProps = {
   params: {
@@ -58,15 +60,26 @@ export default async function Page({ params }: PageProps) {
             }}
           />
         </Suspense>
-        <p className="max-h-40 mt-1 truncate text-xs text-gray-500">
-          Posted by u/{post?.author.username ?? cachedPost.authorUsername}{" "}
-          {formatTimeToNow(new Date(post?.createdAt ?? cachedPost.createdAt))}
-        </p>
-        <h1 className="text-xl font-semibold py-2 leading-6 text-gray-900">
-          {post?.title ?? cachedPost.title}
-        </h1>
+        <div className="sm:w-0 w-full flex-1 bg-white p-4 rounded-sm">
+          <p className="max-h-40 mt-1 truncate text-xs text-gray-500">
+            Posted by {post?.author.username ?? cachedPost.authorUsername}{" "}
+            {formatTimeToNow(new Date(post?.createdAt ?? cachedPost.createdAt))}
+          </p>
+          <h1 className="text-xl font-semibold py-2 leading-6 text-gray-900">
+            {post?.title ?? cachedPost.title}
+          </h1>
 
-        <PostContent content={post?.content ?? cachedPost.content} />
+          <PostContent content={post?.content ?? cachedPost.content} />
+
+          <Suspense
+            fallback={
+              <Loader2 className="h-5 w-5 animate-spin text-zinc-500" />
+            }
+          >
+            {/* @ts-expect-error Server Component */}
+            <CommentsSection postId={post?.id ?? cachedPost.id} />
+          </Suspense>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/PostComment.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Comment, CommentVote, User } from "@prisma/client";
+import UserAvatar from "./UserAvatar";
+import { useRef } from "react";
+import { formatTimeToNow } from "@/lib/utils";
+
+type ExtendedComment = Comment & {
+  votes: CommentVote[];
+  author: User;
+};
+
+type PostCommentProps = {
+  comment: ExtendedComment;
+};
+
+export default function Comment({ comment }: PostCommentProps) {
+  const commentRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <div ref={commentRef} className="flex flex-col">
+      <div className="flex items-center">
+        <UserAvatar
+          user={{
+            name: comment.author.name || null,
+            image: comment.author.image || null,
+          }}
+          className="h-6 w-6"
+        />
+
+        <div className="ml-2 flex items-center gap-x-2">
+          <p className="text-sm font-medium text-gray-900">
+            {comment.author.username}
+          </p>
+
+          <p className="max-h-40 truncate text-xs text-zinc-500">
+            {formatTimeToNow(new Date(comment.createdAt))}
+          </p>
+        </div>
+      </div>
+      <p className="text-sm text-zinc-900 mt-2">{comment.text}</p>
+    </div>
+  );
+}
